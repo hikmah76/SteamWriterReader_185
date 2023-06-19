1c8e4993ec288f37d73e7c4c3c51663de9bb1700
nur hikmah <nur.hikmah190504@gmail.com>
2023-06-19 14:55:13 +0700

    Add project files.

---
+++ SteamWriterReader/SteamWriterReader.cpp
@@ -0,0 +1,22 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string baris;
+	
+	//menulis file dalam mode menulis
+	ofstream outfile;
+	//menunjuk kesebuah nama file
+	outfile.open("contohfile.text");
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+	
+	//unlimited loop untuk menulis
+	while (true) {
+		cout << "- ";
+		//mendapatkan satu karakter dalam satu baris 
+		getline
+	}
+}
